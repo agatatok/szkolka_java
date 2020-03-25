48c3a3901ecf3d4d6897b26c9c7532a8ff4874f0
agatatok <agattok260@student.polsl.pl>
2020-03-25 11:06:19 +0100

    ćwiczenia z vararg

--- src/main/java/pl/accenture/szkolka/Main.java
+++ src/main/java/pl/accenture/szkolka/Main.java
@@ -1,4 +1,23 @@
 package pl.accenture.szkolka;
 
 public class Main {
+
+    public static void main(String[] args) {
+        printPersonInfoV("Jan","Kowalski");
+        printPersonInfoV("Jan1","Kowalski1","wiek: 14","wzrost: 170");
+        printPersonInfoV("Adam","Nowak");
+        printPersonInfoV("Horacy","Nowak");
+    }
+
+    static void printPersonInfoV(String name, String lastName, String ... additionalInfo){
+        System.out.println("Name: "+name);
+        System.out.println("Last name: "+lastName);
+        System.out.println("Additional info:");
+
+        for(String s: additionalInfo) {
+            System.out.println(s);
+        }
+        System.out.println("########");
+    }
+
 }
