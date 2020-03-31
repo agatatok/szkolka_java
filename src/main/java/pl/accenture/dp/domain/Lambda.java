e19a3dac01a735a2e047847f7494b0d21d4afd1f
agatatok <agattok260@student.polsl.pl>
2020-03-31 10:38:39 +0200

    Pierwsze ćwiczenie lambda

--- src/main/java/pl/accenture/dp/domain/Lambda.java
+++ src/main/java/pl/accenture/dp/domain/Lambda.java
@@ -1,29 +1,19 @@
 package pl.accenture.dp.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lambda {
     public static void main(String[] args) {
 
-        MyInterface impl1 = new KlasycznaImplementacja();
-        MyInterface impl2 = new MyInterface() {
-            @Override
-            public String go(String name) {
-                return null;
-            }
-        };
+        List<String> aList = Arrays.asList("a21","a2","a13","a4","b10","b2","c13","c4");
 
-        MyInterface impl3 = name -> name;
+        aList
+                .stream()
+                .filter(s -> s.startsWith("b"))
+                .map(s -> s.toUpperCase())
+                .sorted()
+                .forEach(s->System.out.println(s));
+
     }
-}
-
-@FunctionalInterface
-interface MyInterface {
-    String go(String name);
-}
-
-class KlasycznaImplementacja implements MyInterface {
-    @Override
-    public String go(String name) {
-        return name.toLowerCase();
-    }
-
 }
