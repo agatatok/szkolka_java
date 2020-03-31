4ae589da8cbff9743b231d4785c4d1c7f0a41b8f
agatatok <agattok260@student.polsl.pl>
2020-03-31 14:09:39 +0200

    Zadanie z flatmap

--- src/main/java/pl/accenture/dp/domain/Lambda.java
+++ src/main/java/pl/accenture/dp/domain/Lambda.java
@@ -5,12 +5,14 @@ import lombok.Data;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static pl.accenture.dp.domain.Person.posortujPoMiescie;
 import static pl.accenture.dp.domain.Person.posortujPoNazwisku;
 
 public class Lambda {
     public static void main(String[] args) {
+
 
         List<Person> people = przygotujListeOsob();
         System.out.println("TODO 1");
@@ -20,8 +22,47 @@ public class Lambda {
         System.out.println("TODO 3");
         sortowanieObaJedenOdwrocony(people);
 
+        System.out.println("TODO z flatmap");
+        flatMapuj();
+
+
+ /*
+        //stream map
+        var imionaLudzi = people
+                .stream()
+                .map(p->p.getName())
+                .collect(Collectors.toList());
+        System.out.println(imionaLudzi);
+
+        List<String> ludzie = List.of(
+                "Lukasz-Wyspianski", "Ania-Kowalska", "Jan-Nowak", "Jerzy-Borowik", "Kasia-Nowacka"
+        );
+
+        ludzie
+                .stream()
+                .map(s->new Person(s.substring(0, s.indexOf('-')), s.substring(s.indexOf('-')+1),new Address("Krakow")))
+                .forEach(System.out::println);
+            */
+
 
     }
+
+    private static void flatMapuj() {
+        List<Integer> liczbyPierwsze = List.of(3,5,7);
+        List<Integer> liczbyParzyste = List.of(2,4,6);
+        List<Integer> liczbyNieparzyste = List.of(1,3,5);
+
+        List<List<Integer>> listaDoSplaszczenia = List.of(liczbyPierwsze, liczbyParzyste, liczbyNieparzyste);
+
+        System.out.println(listaDoSplaszczenia);
+
+        listaDoSplaszczenia
+                .stream()
+                .flatMap(x -> x.stream())
+                .sorted()
+                .forEach(System.out::println);
+    }
+
 
     private static void sortowanieJeden(List<Person> people) {
         people
